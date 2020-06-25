bfcc71bdef0227d415d6b91268c679898e572545
Animesh <akanimesh7@gmail.com>
2020-06-25 15:05:26 +0530

    Added Following Functionality 	Added sum function in arithmetic 	Refacored Hello.c

---
+++ Arithmetic.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int sum (int x,int y){
+	return x+y;
+}
+
+int main(){
+	printf("sum of 5 and 6 is %d",sum(5,6));
+	return 0;
+}
--- hello.c
+++ hello.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 
 int main(){
+//modified just for example
 	printf("Hello World !! \n");
 	return 0;
 }
