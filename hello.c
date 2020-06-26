9ce8366a7c211411c848e28bc8eb1c9afb33016e
ChangedLocal <akanimesh7@gmail.com>
2020-06-26 10:08:52 +0530

    Added doWhatYouLike in hello.c

--- hello.c
+++ hello.c
@@ -3,6 +3,14 @@
 void doSomethingAtRemote(){
 	printf("Done Something at remote");
 }
+
+void doWhatYouLike(){
+	printf("Doing what i like ");
+	printf("Doing what i like ");
+	printf("Doing what i like ");
+	printf("Doing what i like ");
+	printf("Doing what i like ");
+}
 int main(){
 //modified just for example
 	printf("Hello World !! \n");
